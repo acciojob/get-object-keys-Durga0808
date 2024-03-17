9ea70925bb9a3bc38d6a2e801c93b43f39fa2f28
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-03-17 15:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Create the student object
+let student = {
+  name: "John"
+};
+
+// Add a method to the Object prototype
+Object.prototype.getKeys = function() {
+  // Get all keys of the object using Object.keys()
+  return Object.keys(this);
+};
+
+// Test the getKeys method
+console.log(student.getKeys()); // Output: ['name']
